cddbd767a137e69eec1e3ed6da72434fce9b3467
gekidokun <84712237+gekidokun@users.noreply.github.com>
2021-08-15 22:47:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,13 +5,14 @@ import traceback
 client = commands.Bot(command_prefix = '!')
 
 
-@client.event
-async def on_ready():
-    print("準備OK")
-    print("---------------------------")
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-    
-@client.command()
+
+@bot.command()
 async def nano_me(ctx):
     await ctx.send('強化完了！力を見せてやれ！')
 
