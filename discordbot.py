5fdadb2c516d05ffa5f454452ab0cc59b7e20b30
gekidokun <84712237+gekidokun@users.noreply.github.com>
2021-08-15 22:58:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
-from discord.ext import commands
-from os import getenv
+import discord
+import random
+import getenv
 import traceback
+from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
 
@@ -11,10 +13,11 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
+    
+@client.command()
 async def nano_me(ctx):
     await ctx.send('強化完了！力を見せてやれ！')
+
 
 
 token = getenv('DISCORD_BOT_TOKEN')
